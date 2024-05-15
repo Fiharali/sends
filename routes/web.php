6ab557b980f255b365cf029d6b24b05fbdc61c85
fihar abdelali <fiharali0@gmail.com>
2024-05-15 12:00:58 +0100

    add sending message test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\AdminController;
+use App\Http\Controllers\admin\TestController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [AdminController::class , 'index'])->name('dashboard');
+Route::get('/test', [TestController::class , 'index'])->name('test');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
